a0cfb12c09f56188dc17c1b0c21d4ecb2adf232c
MinhKhoa2719 <minhkhoa2719@gmail.com>
2020-11-27 10:00:00 +0700

    push lan 4

--- ui/src/App.js
+++ ui/src/App.js
@@ -23,7 +23,7 @@ export default class PersonList extends React.Component {
   render() {
     return (
       <ul>
-        khoa minh test test test lan 3
+        khoa minh test test test test lan 4
         { this.state.test.map(test => <li>{test}</li>)}
       </ul>
     )
